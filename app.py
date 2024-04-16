3f88434c9909e1ee20e812e03c686459345a65ec
giovannicanedoli <gio.canedoli02@gmail.com>
2024-04-16 22:30:54 +0200

    aggiunta roba

--- app.py
+++ app.py
@@ -1,76 +1,159 @@
-from flask import Flask,render_template, url_for, redirect, request
+from flask import Flask,render_template, url_for, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
+
+
+
 from flask_login import LoginManager, UserMixin, login_user, logout_user
+from datetime import timedelta
 
 
+from utils import *
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-app.config["SECRET_KEY"] = "HOMEWORK LTW" #random secret key
-db = SQLAlchemy() #inizializzo il db
+app.config["SECRET_KEY"] = "HOMEWORK LTW"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
-
+db = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-
-#il tutorial: https://www.geeksforgeeks.org/how-to-add-authentication-to-your-app-with-flask-login/
-#mette i modelli direttamente dentro al file app.py, non credo sia una buona pratica. Per adesso mi sa 
-# che li possiamo lasciare qui, in caso poi spostiamoli.
 class Users(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(250), unique=True, nullable=False)
-    password = db.Column(db.String(250), nullable=False)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)    
+    username = db.Column(db.String(30),nullable = False, unique = True)    
+    password = db.Column(db.String(250),nullable = False)
+
+    def __init__(self, username = None,password = None):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User {self.username}, password {self.password}>'
+    
+class Cities(db.Model):
+    __tablename__ = 'cities'
+    id = db.Column(db.Integer, primary_key=True)    
+    like_messi = db.Column(db.Integer)    
+    nome = db.Column(db.String(250), nullable = False)
+    paese = db.Column(db.String(250), nullable = False)
+    photo = db.Column(db.String(250), nullable = False)
+
+    def __init__(self,like = 0, nome = None, paese = None, photo = None):
+        self.like = like
+        self.nome = nome
+        self.paese = paese
+        self.photo = photo
+
+    def __repr__(self):
+        return f'<nome {self.nome}, paese {self.paese}>, like {self.like}'
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+    id = db.Column(db.Integer, primary_key=True)   
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    cities_id = db.Column(db.Integer, db.ForeignKey('cities.id'))
+
+    #uselist = False -> un record è associato ad un record delle classi sopra
+
+    users = db.relationship("Users", backref=db.backref("users", uselist=False))
+    cities = db.relationship("Cities", backref=db.backref("cities", uselist=False))
 
 
 db.init_app(app)
 
+
 with app.app_context():
     db.create_all()
-
 
 # Creates a user loader callback that returns the user object given an id
 @login_manager.user_loader
 def loader_user(user_id):
     return Users.query.get(user_id)
 
+    
 @app.route("/")
 def main_route():
     return render_template("index.html")
-
-@app.route("/home")
-def renderize():
-    return render_template("login.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        user = Users.query.filter_by(username=request.form.get("username")).first()
-        if user.password == request.form.get("password"):
-            login_user(user)
-            return redirect(url_for("home"))
-        
-    return render_template("login.html")
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        user = Users(username=request.form.get("username"),
-                     password=request.form.get("password"))
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for("login"))
-    return render_template("sign_up.html")
+
+        username = request.form.get('username_input')
+        password = request.form.get("password_input1")
+        password_verify = request.form.get("password_input1")
+        username = request.form.get("username_input")
+    
+        password_ok = verify_password(password_verify)
+
+        user = Users.query.filter_by(username = username).first()
+
+        if user:
+            return render_template("signup.html", user_alive = True)
+
+        if password == password_verify and password_ok:
+            user = Users(username, password)
+            db.session.add(user)
+            db.session.commit()
+
+            session.permanent = True
+            session['username'] = username
+            session['password'] = password
+
+            return redirect(url_for("main_route"))
+        else:
+            return render_template("signup.html", password_ok=password_ok)
+    elif 'username' in session and 'password' in session:
+        return redirect(url_for("main_route"))
+    else:
+        return render_template("signup.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username_input")
+        password_verify = request.form.get("password_input")
+        user = Users.query.filter_by(username = username).first()
+        
+        #qui eventualmente otp
+        if user and user.password == password_verify:
+            login_user(user)                #AttributeError: 'Users' object has no attribute 'is_active'    non so in realtà a che serva sta funzione
+            return redirect(url_for("main_route"))
+        
+        elif not user:
+            return render_template("login.html", something_failed = True, user_not_found = True)
+        
+        else:
+            return render_template("login.html", something_failed = True, user_not_found = False)
+        
+    elif 'username' in session and 'password' in session:
+        return redirect(url_for("main_route"))
+    
+    else:
+        return render_template("login.html", something_failed = False)
+
+@app.route("/home")
+def renderize():
+    return render_template("index.html")
+
+@app.route('/forget')
+def forgotpasswd():
+    return render_template('forgot.html')
+
+@app.route('/<something>')
+def goto(something):
+    return redirect(url_for('main_route'))
+
+'''
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("home"))
+'''
 
-
-#runna l'app
 if __name__ == '__main__':
     app.run(debug=True)

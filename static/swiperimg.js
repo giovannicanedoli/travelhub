b4cd14fb4ec3d2b53c60c6a5aba8e34a0ca40207
giovannicanedoli <gio.canedoli02@gmail.com>
2024-05-20 17:07:01 +0200

    creata classe swipercube2a, la secodaa slidddee ddi aalcue iagii o è renderizzata correttamente

--- static/swiperimg.js
+++ static/swiperimg.js
@@ -53,6 +53,32 @@ var swiper2 = new Swiper(".swiperCubic", {
   }
 });
 
+var swiper2b = new Swiper(".swiperCubic2", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    // when window width is >= 760px
+    760: {
+      // slidesPerView: 2,
+      // spaceBetween: 20
+    },
+    // when window width is >= 1050px
+    1050: {
+      // slidesPerView: 3,
+      // spaceBetween: 30
+    }
+  }
+});
+
 var swiper3 = new Swiper(".swiperReview", {
   // parallax: true,
   pagination: {
